5a9fa2edf0827125cc01b8676c16b9392ca35037
Chinmay Barman <chinmaybarman0003@gmail.com>
2026-07-09 12:06:47 +0530

    Time: 24 ms (5.79%) | Memory: 47 MB (5.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,12 +1,38 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
+        if(s.length()!=t.length()){
             return false;
         }
-        char c1[] = s.toCharArray();
-        char c2[] = t.toCharArray();
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        return Arrays.equals(c1,c2);
+        HashMap<Character,Integer> maps = new HashMap<>();
+        HashMap<Character,Integer> mapt = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(maps.containsKey(s.charAt(i))){
+                int count = maps.get(s.charAt(i));
+                maps.put(s.charAt(i),++count);
+            }
+            else{
+                maps.put(s.charAt(i),1);
+            }
+        }
+        for(int i=0;i<t.length();i++){
+            if(mapt.containsKey(t.charAt(i))){
+                int count = mapt.get(t.charAt(i));
+                mapt.put(t.charAt(i),++count);
+            }
+            else{
+                mapt.put(t.charAt(i),1);
+            }
+        }
+        for(Character key: maps.keySet()){
+            if(mapt.containsKey(key)){
+                if(!maps.get(key).equals(mapt.get(key))){
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
     }
 }
