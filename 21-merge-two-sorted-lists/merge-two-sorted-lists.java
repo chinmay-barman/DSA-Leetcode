1b3411ef47ffb166ba87f99cc2d2255748e0e32b
Chinmay Barman <chinmaybarman0003@gmail.com>
2026-07-18 00:42:07 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (21.01%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,36 +10,35 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode p1 = list1;
-        ListNode p2 = list2;
-        ListNode ans = new ListNode(0);
-        ListNode pointer = ans;
-        while(p1!=null && p2!=null){
-            if(p1.val<p2.val){
-                ListNode newNode = new ListNode(p1.val);
-                pointer.next = newNode;
-                pointer = newNode;
-                p1 = p1.next;
+        ListNode first = list1;
+        ListNode second = list2;
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+        while(first!=null && second!=null){
+            ListNode newNode;
+            if(first.val<second.val){
+                newNode = new ListNode(first.val);
+                first = first.next;
             }
             else{
-                ListNode newNode = new ListNode(p2.val);
-                pointer.next = newNode;
-                pointer = newNode;
-                p2 = p2.next;
+                newNode = new ListNode(second.val);
+                second = second.next;
             }
+            temp.next = newNode;
+            temp = temp.next;
         }
-        while(p1!=null){
-            ListNode newNode = new ListNode(p1.val);
-            pointer.next = newNode;
-            pointer = newNode;
-            p1 = p1.next;
+        while(first!=null){
+            ListNode newNode = new ListNode(first.val);
+            temp.next = newNode;
+            temp = temp.next;
+            first = first.next;
         }
-        while(p2!=null){
-            ListNode newNode = new ListNode(p2.val);
-            pointer.next = newNode;
-            pointer = newNode;
-            p2 = p2.next;
+        while(second!=null){
+            ListNode newNode = new ListNode(second.val);
+            temp.next = newNode;
+            temp = temp.next;
+            second = second.next;
         }
-        return ans.next;
+        return dummy.next;
     }
 }
