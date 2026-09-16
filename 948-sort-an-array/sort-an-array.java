7190170af257ed43177b6ea0fd58183b62f1175f
Chinmay Barman <chinmaybarman0003@gmail.com>
2026-09-17 00:02:51 +0530

    Time: 64 ms (14.86%) | Memory: 74.8 MB (97.03%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -3,39 +3,39 @@ class Solution {
         mergeSort(nums,0,nums.length-1);
         return nums;
     }
-    public void mergeSort(int nums[],int left, int right){
-        if(left==right){
+    public void mergeSort(int nums[], int left, int right){
+        if(left>=right){
             return;
         }
         int mid = left+(right-left)/2;
         mergeSort(nums,left,mid);
-        mergeSort(nums,mid+1,right);
-        merge(nums,left,mid,right);
+        mergeSort(nums,mid+1, right);
+        merge(nums, left, mid, right);
     }
-    public void merge(int nums[],int left,int mid,int right){
-        ArrayList<Integer> arr = new ArrayList<>();
-        int i=left;
-        int j=mid+1;
+    public void merge(int nums[], int left, int mid, int right){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i = left;
+        int j = mid+1;
         while(i<=mid && j<=right){
             if(nums[i]<=nums[j]){
-                arr.add(nums[i]);
+                ans.add(nums[i]);
                 i++;
             }
             else{
-                arr.add(nums[j]);
+                ans.add(nums[j]);
                 j++;
             }
         }
         while(i<=mid){
-            arr.add(nums[i]);
+            ans.add(nums[i]);
             i++;
         }
         while(j<=right){
-            arr.add(nums[j]);
+            ans.add(nums[j]);
             j++;
         }
-        for(int k=0;k<arr.size();k++){
-            nums[k+left] = arr.get(k);
+        for(int k=0;k<ans.size();k++){
+            nums[k+left] = ans.get(k);
         }
     }
 }
